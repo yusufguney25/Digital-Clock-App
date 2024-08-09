2f3bace5a26b4bf6832f1c7ad283cde6f93f1ddc
yusufguney25 <120314851+yusufguney25@users.noreply.github.com>
2024-08-09 10:07:42 +0300

    Initial commit

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,44 @@
+//React hooks blog
+import React,{useState,useEffect} from "react";
+function DigitalClock(){
+    const [time,setTime]=useState(new Date());
+    useEffect(()=>{
+        const intervalId = setInterval(()=>{
+            setTime(new Date());
+        }, 1000);
+
+        return ()=>{
+            clearInterval(intervalId);
+        }
+    },[]);
+    //Format Time Blog
+    function formatTime(){
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridiem =  hours >=12 ? "PM" : "AM"; //Post meridian(PM) - ante meridian(AM)
+
+        hours = hours % 12 || 12 ;
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}:${meridiem}`
+    }
+
+    function padZero(number){
+        return (number<10 ? "0" : "")+ number;
+    };
+
+    return (
+    <>
+        <div className="clock-container">
+            <div className="clock">
+                <span>{formatTime()}</span>
+            </div>
+        </div>
+    
+    </>
+    
+    
+    
+    );
+}
+export default DigitalClock;
